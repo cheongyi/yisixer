05be89080dff9ead4e8bba9793568ffa054d130f
chenhongyi <cheongyi@ChenHongyi.local>
2017-11-09 16:25:07 +0800

    后天11.11

--- server/include/define.hrl
+++ server/include/define.hrl
@@ -1,36 +1,41 @@
 -ifdef (debug).
-    -define(IS_DEBUG, true).
-    -define(DEBUG(Msg, Args), io:format("[debug] - " ++ Msg, Args)).
-    -define(FORMAT(Msg, Args), io:format(Msg, Args)).
+    -define (IS_DEBUG,          true).
+    -define (DEBUG(Msg, Args), 
+        io:format(
+            "[debug] - from player:~p ~p(~p) ~p~n" ++ Msg, 
+            [?MODULE, ?LINE, erlang:localtime() | Args]
+        )
+    ).
+    -define (FORMAT(Msg, Args), io:format(Msg, Args)).
 -else.
-    -define(IS_DEBUG, false).
-    -define(DEBUG(Msg, Args), ok).
-    -define(FORMAT(Msg, Args), ok).
+    -define (IS_DEBUG,          false).
+    -define (DEBUG(Msg,  Args), ok).
+    -define (FORMAT(Msg, Args), ok).
 -endif.
 
 
--define(GAME_LOG_DIR, "./log/").
--define(DATA_DIR, "./data/").
--define(WAR_REPORT_DIR, ?DATA_DIR ++ "war_report/").
+-define (GAME_LOG_DIR, "./log/").
+-define (DATA_DIR, "./data/").
+-define (WAR_REPORT_DIR, ?DATA_DIR ++ "war_report/").
 
 -define (SHUTDOWN_WORKER,       16#ABCDEF0).        % 一个工作进程将怎样被终止
 -define (SHUTDOWN_SUPERVISOR,   infinity).          % 一个监督进程将怎样被终止
 
 
 %% 日志写入
--define(INFO(Msg, Args),    game_log:write(info,    Msg, Args)).
--define(ERROR(Msg, Args),   game_log:write(error,   Msg, Args)).
--define(WARNING(Msg, Args), game_log:write(warning, Msg, Args)).
+-define (INFO(Msg,    Args), game_log:write(info,    Msg, Args)).
+-define (ERROR(Msg,   Args), game_log:write(error,   Msg, Args)).
+-define (WARNING(Msg, Args), game_log:write(warning, Msg, Args)).
 
 
 %% 获取应用环境变量值
--define(GET_ENV(Key, Default), (
+-define (GET_ENV(Key, Default),     (
     case application:get_env(Key) of
         {ok, Val} -> Val;
         undefined -> Default
     end
 )).
--define(GET_ENV_STR(Key, Default), (
+-define (GET_ENV_STR(Key, Default), (
     case application:get_env(Key) of
         {ok, Val} when is_list(Val)     -> Val;
         {ok, Val} when is_atom(Val)     -> atom_to_list(Val);
@@ -38,7 +43,7 @@
         undefined                       -> Default
     end
 )).
--define(GET_ENV_INT(Key, Default), (
+-define (GET_ENV_INT(Key, Default), (
     case application:get_env(Key) of
         {ok, Val} when is_integer(Val)  -> Val;
         {ok, Val} when is_list(Val)     -> list_to_integer(Val);
@@ -46,7 +51,7 @@
         undefined                       -> Default
     end
 )).
--define(GET_ENV_ATOM(Key, Default), (
+-define (GET_ENV_ATOM(Key, Default),(
     case application:get_env(Key) of
         {ok, Val} when is_atom(Val)     -> Val;
         {ok, Val} when is_list(Val)     -> list_to_atom(Val);
---
+++ server/src/lib/lib_misc.erl
@@ -0,0 +1,49 @@
+-module (lib_misc).
+
+-author ("CHEONGYI").
+
+-copyright ("Copyright © 2017 YiSiXEr").
+
+-compile (export_all).
+
+-export ([
+    key_index_of_record/2,                      % 获取键在记录的索引
+    index_of_tuple/2,                           % 获取元素在元组的索引
+    index_of_list/2                             % 获取元素在列表的索引
+]).
+
+
+
+%%% ========== ======================================== ====================
+%%% External   API
+%%% ========== ======================================== ====================
+%%% @doc    获取键在记录的索引
+key_index_of_record (Key, Record) ->
+    RecordName  = element(1, Record),
+    FieldList   = record_info(fields, RecordName),
+    index_of_list_3(Element, FieldList, 2).
+
+%%% @doc    获取元素在元组的索引
+index_of_tuple (Element, Tuple) ->
+    index_of_list_3(Element, tuple_to_list(Tuple), 1).
+
+
+%%% @doc    获取元素在列表的索引
+index_of_list (Element, List) ->
+    index_of_list_3(Element, List, 1).
+
+index_of_list_3 (Element, [Element | List], Index) ->
+    Index;
+index_of_list_3 (Element, [_ | List], Index) ->
+    index_of_list_3(Element, List, Index + 1);
+index_of_list_3 (Element, [], Index) ->
+    0.
+
+
+
+
+
+
+
+
+
