c6354476d627dd02e8b00b60e40d2cf76c2f2135
chenhongyi <cheongyi@ChenHongyi.local>
2018-03-19 22:06:03 +0800

    start.sh

--- server/start.sh
+++ server/start.sh
@@ -8,16 +8,16 @@ erl \
 -name yisixer@127.0.0.1 \
 -setcookie the_14x2_cookie \
 -game \
- gateway_node           '"gw@127.0.0.1"' \
- policy_server          '"false"' \
- server_port            '"1428"' \
- mysql_host             '"127.0.0.1"' \
- mysql_port             '3306' \
- mysql_username         '"root"' \
- mysql_password         '"ybybyb"' \
- mysql_database         '"yisixer"' \
- build_code_db          'true' \
- vsn                    '"2017052001"'
+    gateway_node            '"gw@127.0.0.1"' \
+    policy_server           '"false"' \
+    server_port             '"1428"' \
+    mysql_host              '"127.0.0.1"' \
+    mysql_port              '3306' \
+    mysql_username          '"root"' \
+    mysql_password          '"ybybyb"' \
+    mysql_database          '"yisixer"' \
+    build_code_db           'true' \
+    vsn                     '"2017052001"'
 
 
 
