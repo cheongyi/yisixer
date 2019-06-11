560832f51d2e7f51a72853b63199dab73126116e
cheongyi <chenhy@blizzmi.com>
2019-06-11 21:06:09 +0800

    红包算法优化和显示

--- server/src/hong_bao.erl
+++ server/src/hong_bao.erl
@@ -1,5 +1,7 @@
 -module (hong_bao).
 
+%%% @doc    参考链接：https://www.cnblogs.com/dreign/p/4610766.html
+
 -author     ("CHEONGYI").
 -date       ({2019, 06, 11}).
 -vsn        ("1.0.0").
@@ -17,19 +19,19 @@
 %%% ========== ======================================== ====================
 %%% External   API
 %%% ========== ======================================== ====================
-test (Total, Number) when Total > 0 andalso Number > 0 ->
-    TotalFen    = Total * ?YUAN_TO_FEN,
+test (Total, Number) when Total > 0 andalso Number > 0 andalso Total * ?YUAN_TO_FEN >= Number->
+    TotalFen    = trunc(Total * ?YUAN_TO_FEN),
     loop_open(TotalFen, Number, 1, {TotalFen, 0}, []).
 
 loop_open (TotalFen, Index, Index, {Min, Max}, List) ->
     Money       = TotalFen,
     NewTotal    = TotalFen - Money,
-    io:format("~2..0w Money $ ~-4.2.0f === Total $ ~-4.2.0f~n", [Index, Money / ?YUAN_TO_FEN, NewTotal / ?YUAN_TO_FEN]),
+    io:format("(~2..0w) $ ~6.2.0f +++ $ ~6.2.0f~n", [Index, Money / ?YUAN_TO_FEN, NewTotal / ?YUAN_TO_FEN]),
     io:format("~nMin:~p~nMax:~p~n", [min(Min, TotalFen) / ?YUAN_TO_FEN, max(Max, TotalFen) / ?YUAN_TO_FEN]),
     io:format("~p = ~p~n", [lists:sum([TotalFen | List]), [TotalFen | List]]);
 loop_open (TotalFen, Number, Index, {Min, Max}, List) ->
     SafeTotal   = (TotalFen - (Number - Index) * ?MIN_1_FEN) div (Number - Index),   % 随机安全上限
-    Money       = lib_misc:random_number(SafeTotal),
+    Money       = max(?MIN_1_FEN, lib_misc:random_number(SafeTotal)),
     NewTotal    = TotalFen - Money,
-    io:format("~2..0w Money $ ~-4.2.0f === Total $ ~-4.2.0f~n", [Index, Money / ?YUAN_TO_FEN, NewTotal / ?YUAN_TO_FEN]),
+    io:format("(~2..0w) $ ~6.2.0f +++ $ ~6.2.0f~n", [Index, Money / ?YUAN_TO_FEN, NewTotal / ?YUAN_TO_FEN]),
     loop_open(NewTotal, Number, Index + 1, {min(Min, Money), max(Max, Money)}, [Money | List]).
